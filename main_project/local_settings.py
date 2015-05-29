a4a194a1799515918deff85ef03acc2f3a496fd3
Ubuntu <ubuntu@ip-172-31-22-100.us-west-2.compute.internal>
2015-05-29 18:21:33 +0000

    adds local_settings file for EC2 instance

--- main_project/local_settings.py
+++ main_project/local_settings.py
@@ -1,2 +1,19 @@
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True  
+
+import os
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ec2_deploy_test',
+        'HOST': 'localhost',
+        'USER': 'ec2_deploy_test',
+        'PASSWORD': 'cccpussr'
+    }
+}
+
+# Base dir is up two levels
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# static file dir will live in base dir after collect static is run
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
